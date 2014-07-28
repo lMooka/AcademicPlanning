47b073fdfe43c50e819e1fc6a3c7c3a3d1f57a37
ramonbrbs <ramonflmr@gmail.com>
2014-07-28 00:59:29 -0300

    Dump do BD

--- dbdump/dump.sql
+++ dbdump/dump.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tempo de Geração: 08/07/2014 às 00h12min
+-- Tempo de Geração: 28/07/2014 às 05h52min
 -- Versão do Servidor: 5.5.20
 -- Versão do PHP: 5.3.9
 
@@ -49,14 +49,14 @@ CREATE TABLE IF NOT EXISTS `docente` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=5 ;
 
 --
 -- Extraindo dados da tabela `docente`
 --
 
 INSERT INTO `docente` (`id`, `nome`) VALUES
-(1, 'Fred Durão');
+(2, 'Teste Teste 2');
 
 -- --------------------------------------------------------
 
@@ -72,14 +72,14 @@ CREATE TABLE IF NOT EXISTS `horario` (
   `turma_id` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `index_foreignkey_horario_turma` (`turma_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=40 ;
 
 --
 -- Extraindo dados da tabela `horario`
 --
 
 INSERT INTO `horario` (`id`, `dia`, `inicio`, `fim`, `turma_id`) VALUES
-(1, 'seg', '20:30', '22:30', NULL);
+(38, '1', '10:00', '12:00', 34);
 
 -- --------------------------------------------------------
 
@@ -90,17 +90,24 @@ INSERT INTO `horario` (`id`, `dia`, `inicio`, `fim`, `turma_id`) VALUES
 CREATE TABLE IF NOT EXISTS `materia` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `credito` int(11) unsigned DEFAULT NULL,
+  `credito` int(11) DEFAULT NULL,
   `ref` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `obrigatoria` tinyint(1) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=9 ;
 
 --
 -- Extraindo dados da tabela `materia`
 --
 
-INSERT INTO `materia` (`id`, `nome`, `credito`, `ref`) VALUES
-(1, 'Laboratório de Programação WEb', 2, 'MATC84');
+INSERT INTO `materia` (`id`, `nome`, `credito`, `ref`, `obrigatoria`) VALUES
+(1, 'Laboratório de Programação WEB', 2, 'MATC84', NULL),
+(3, 'Calculo Z', 4, 'MATA01', 1),
+(4, 'Calculo Z', 4, 'MATA01', 1),
+(5, 'Calculo Z', 4, 'MATA01', 1),
+(6, 'Calculo Z', 4, 'MATA01', 1),
+(7, 'Calculo Z', 4, 'MATA01', 1),
+(8, 'Calculo H', 6, 'MATA55', NULL);
 
 -- --------------------------------------------------------
 
@@ -113,21 +120,24 @@ CREATE TABLE IF NOT EXISTS `turma` (
   `materia_id` int(11) unsigned DEFAULT NULL,
   `docente_id` int(11) unsigned DEFAULT NULL,
   `curso_id` int(11) unsigned DEFAULT NULL,
-  `materia` tinyint(1) unsigned DEFAULT NULL,
-  `docente` tinyint(1) unsigned DEFAULT NULL,
-  `curso` tinyint(1) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `index_foreignkey_turma_materia` (`materia_id`),
   KEY `index_foreignkey_turma_docente` (`docente_id`),
   KEY `index_foreignkey_turma_curso` (`curso_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=39 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=49 ;
 
 --
 -- Extraindo dados da tabela `turma`
 --
 
-INSERT INTO `turma` (`id`, `materia_id`, `docente_id`, `curso_id`, `materia`, `docente`, `curso`) VALUES
-(22, 1, 1, NULL, NULL, NULL, NULL);
+INSERT INTO `turma` (`id`, `materia_id`, `docente_id`, `curso_id`) VALUES
+(34, 1, NULL, NULL),
+(43, 5, NULL, NULL),
+(44, 1, NULL, NULL),
+(45, 1, NULL, NULL),
+(46, 1, NULL, NULL),
+(47, 4, NULL, NULL),
+(48, 8, NULL, NULL);
 
 --
 -- Restrições para as tabelas dumpadas
